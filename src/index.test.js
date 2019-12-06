d1300661ba9bd795392445d40baaa9f37a7b5903
Oliver Köhler <oliver.koehler@gileo.de>
2019-12-06 12:23:26 +0100

    Add test.

---
+++ src/index.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Game from "./index";
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Game />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Game />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
